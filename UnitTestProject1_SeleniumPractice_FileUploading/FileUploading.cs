887268917b0ddd1f7db3f14dfc11b781b37b6975
Pataballa N V Lakshminarayana <narayana9573944031@gmail.com>
2025-01-20 21:42:38 +0530

    Add project files.

---
+++ UnitTestProject1_SeleniumPractice_FileUploading/FileUploading.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Threading;
+
+namespace UnitTestProject1_SeleniumPractice_FileUploading
+{
+    [TestClass]
+    public class FileUploading
+    {
+        public static IWebDriver _Driver;
+        [TestMethod]
+        public void FileUploadingTestCase()
+        {
+            _Driver = new ChromeDriver();
+            _Driver.Navigate().GoToUrl("https://the-internet.herokuapp.com/upload");
+            _Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(12);
+            var filepath = _Driver.FindElement(By.XPath("//input[@id='file-upload']"));
+            filepath.SendKeys(@"C:\Users\lakshminarayana.B2BSOFTECH\Desktop\PathVia\Test Process.JPG");
+            _Driver.FindElement(By.XPath("//input[@id='file-submit']")).Click();
+            if(filepath !=null)
+            {
+                Console.WriteLine("File Uploaded Successfully..!");
+            }
+            else
+            {
+                Console.WriteLine("File Not Uploaded");
+            }
+            Thread.Sleep(10000);
+            _Driver.Quit();
+        }
+    }
+}
---
+++ UnitTestProject1_SeleniumPractice_FileUploading/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestProject1_SeleniumPractice_FileUploading")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTestProject1_SeleniumPractice_FileUploading")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("42a831cb-4d2b-49aa-9600-297034820faf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
